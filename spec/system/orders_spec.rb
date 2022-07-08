79692bf4d5344496ad23d26a027f199d233ec2a2
Alan Mendicutti <alan.mendicuttimldn@gmail.com>
2022-07-08 19:13:23 +0000

    Finishing stimulus test

--- spec/system/orders_spec.rb
+++ spec/system/orders_spec.rb
@@ -10,6 +10,20 @@ RSpec.describe "Orders", type: :system do
             click_on 'Checkout'
 
             expect(page).to have_content("Please enter your details")
+            expect(page).to_not have_field("Credit card number")
+            expect(page).to_not have_field("Expiration date")
+            expect(page).to_not have_field("Po number")
+
+            select 'Credit card', from: 'Pay type'
+
+            expect(page).to have_field("Credit card number")
+            expect(page).to have_field("Expiration date")
+
+            select 'Purchase order', from: 'Pay type'
+            expect(page).to_not have_field("Credit card number")
+            expect(page).to_not have_field("Expiration date")
+            
+            expect(page).to have_field("Po number")
         end
     end
 end
